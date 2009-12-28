7f118da47c8a396ebe6495040a5a8472a5a9e3ff
marcus bueschleb <ligi@ligi.de>
2009-12-28 12:59:20 +0100

    fill with the right param names

--- android/src/org/ligi/android/dubwise/FlightSettingsTopicListActivity.java
+++ android/src/org/ligi/android/dubwise/FlightSettingsTopicListActivity.java
@@ -1,6 +1,8 @@
 package org.ligi.android.dubwise;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
@@ -17,11 +19,22 @@ public class FlightSettingsTopicListActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		menu_items=new String[2];
-		menu_items[0]=""+MKProvider.getMK().params.tab_stringids[0];
+		int act_paramset=MKProvider.getMK().params.act_paramset;
+		
+		
+		  menu_items=new String[MKProvider.getMK().params.tab_stringids.length];
+	        for (int i=0;i<menu_items.length;i++)
+	        menu_items[i]=getString(DUBwiseStringHelper.table[MKProvider.getMK().params.tab_stringids[i]]);
+	    /*
+		menu_items=new String[MKProvider.getMK().params.field_stringids[act_paramset][getIntent().getIntExtra("foo",0)]];
+		
+		
+		
+		for (int i=0;i<menu_items.length;i++)
+		menu_items[i]=getString(DUBwiseStringHelper.table[MKProvider.getMK().params.field_stringids[act_paramset][0]]);
+		*/      
 		this.setListAdapter(new ArrayAdapter<String>(this,
 		 android.R.layout.simple_list_item_1, menu_items));
-
 	}
 
 	public void log(String msg) {
@@ -37,7 +50,10 @@ public class FlightSettingsTopicListActivity extends ListActivity {
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-
+		Intent edit_intent= new Intent( this, FlightSettingsTopicEditActivity.class );
+		edit_intent.putExtra("topic",position );
+		startActivity( edit_intent);
+        
 				// Start the activity
 
 	}
