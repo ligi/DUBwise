66e475a04d1d3f353e30c81758e9c0a2c550d32e
marcus bueschleb <ligi@ligi.de>
2010-01-15 02:17:20 +0100

    visual stuff

--- android/src/org/ligi/android/dubwise/LCDView.java
+++ android/src/org/ligi/android/dubwise/LCDView.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.graphics.*;
 import android.graphics.Paint.Align;
@@ -12,7 +14,7 @@ import android.graphics.Paint.Align;
 // not working atm - import org.bluez.*;
 import org.ligi.ufo.*;
 
-public class LCDView extends View implements DUBwiseDefinitions, OnSharedPreferenceChangeListener
+public class LCDView extends View implements DUBwiseDefinitions
 
 {
 	private Paint mPaint = new Paint();
@@ -28,14 +30,12 @@ public class LCDView extends View implements DUBwiseDefinitions, OnSharedPrefere
 		// needed to get Key Events
 		setFocusable(true);
 	
-		settings.registerOnSharedPreferenceChangeListener(this);
-		refresh_settings(settings);
 	}
 
     float[] line_points;
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		char_height=w/15;
+		char_height=w/10;
 		char_width=w/20;
 	}
 
@@ -43,14 +43,12 @@ public class LCDView extends View implements DUBwiseDefinitions, OnSharedPrefere
 	int char_width=0;
 	@Override
 	protected void onDraw(Canvas canvas) {
-
-		
 		Paint textPaint=new Paint();
 		
-		textPaint.setColor(0xFF42DD42);
+		textPaint.setColor(0xFF00ff01);		
 		textPaint.setAntiAlias(false);
-		textPaint.setColor(0xFF000000);
-		textPaint.setTextSize(char_height-5);
+		
+		textPaint.setTextSize(char_height*0.65f);
 		textPaint.setTextAlign(Align.CENTER);
 		//textPaint.setShadowLayer(2, 2, 2, 0xFFFFFFFF);
 		textPaint.setFakeBoldText(true);
@@ -58,27 +56,16 @@ public class LCDView extends View implements DUBwiseDefinitions, OnSharedPrefere
 			{
 			for (int c=0;c<20;c++)
 			{
-				mPaint.setColor(0xFF42DD43);
+				mPaint.setColor(0xFF004000);
 			
-				canvas.drawRect(new Rect(c*(this.getWidth()/20)+1 , char_height*line+1, (c+1)*(this.getWidth()/20)-2  , char_height*(line+1)-2), mPaint);
+				canvas.drawRect(new RectF((this.getWidth()*c/20.0f)+1.0f , char_height*line+1, (this.getWidth()*(c+1)/20.0f)-2.0f  , char_height*(line+1)-2), mPaint);
 				//canvas.drawRect(new Rect(c*(this.getWidth()/20) , char_height*line, (c+1)*(this.getWidth()/20)  , char_height*(line+1)), mPaint);
 				//canvas.drawText(""+MKProvider.getMK().LCD.get_act_page()[line], 10, 10+line*mPaint.getTextSize(), mPaint);
 				//canvas.drawRect(new Rect(chr*(this.getWidth()/20) , char_height*line, (chr+1)*(this.getWidth()/20)  , char_height*(line+1)), mPaint);
-				canvas.drawText("" + MKProvider.getMK().LCD.get_act_page()[line].charAt(c), c*(this.getWidth()/20)+this.getWidth()/40 , char_height*(line)-mPaint.getFontMetrics().bottom-2, textPaint);
+				canvas.drawText("" + MKProvider.getMK().LCD.get_act_page()[line].charAt(c), (float)c*(this.getWidth()/20.0f)+this.getWidth()/40.0f , char_height*(line)+3.0f*char_height/4.0f, textPaint);
 				
 			}}
 		invalidate();
 	}
-
-
 	
-	public void onSharedPreferenceChanged(SharedPreferences _settings, String arg1) {
-		refresh_settings(_settings);
-		System.out.println(" pref change !!!");
-	}
-
-	public void refresh_settings(SharedPreferences _settings) {
-		MKProvider.getMK().user_intent = USER_INTENT_GRAPH;			
-	}
-
 }
