f8acaff48eba82f57e659dfd45f9ce768784fde4
marcus bueschleb <ligi@ligi.de>
2010-02-03 17:57:12 +0100

    options menu for the Map

--- android/src/org/ligi/android/dubwise/map/DUBwiseMap.java
+++ android/src/org/ligi/android/dubwise/map/DUBwiseMap.java
@@ -3,16 +3,25 @@ package org.ligi.android.dubwise.map;
 import org.ligi.android.dubwise.R;
 import org.ligi.android.dubwise.R.id;
 import org.ligi.android.dubwise.R.layout;
+import org.ligi.android.dubwise.con.MKProvider;
+import org.ligi.android.dubwise.graph.GraphSettingsActivity;
 import org.ligi.android.dubwise.helper.ActivityCalls;
+import org.ligi.ufo.MKCommunicator;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ZoomControls;
@@ -24,6 +33,10 @@ import com.google.android.maps.MapView;
 
 public class DUBwiseMap extends MapActivity implements LocationListener {
 	
+	private static final int MENU_SETTINGS = 0;
+	private static final int MENU_ZOOM_KOPTER = 1;
+	private static final int MENU_ZOOM_HOME = 2;
+	
 	LinearLayout linearLayout;
 	MapView mapView;
 	ZoomControls mZoom;
@@ -33,6 +46,8 @@ public class DUBwiseMap extends MapActivity implements LocationListener {
 	@Override 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		MKProvider.getMK().user_intent=MKCommunicator.USER_INTENT_GPSOSD;	
 		//mapView = new MapView(this, "05yE31rWh6bR5W3A9cOq2www1gYQr2XDq5ACbSg"); /* debug */
 		
 		
@@ -78,8 +93,7 @@ public class DUBwiseMap extends MapActivity implements LocationListener {
 		 mapView.getOverlays().add(overlay);
 		 
 		 LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		 lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, this);
-		 
+		 lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 5.0f, this);
 		 
 	}
 
@@ -99,7 +113,7 @@ public class DUBwiseMap extends MapActivity implements LocationListener {
 		if (location != null) {
 			double lat = location.getLatitude();
 			double lng = location.getLongitude();
-			GeoPoint p = new GeoPoint((int) lat * 1000000, (int) lng * 1000000);
+			GeoPoint p = new GeoPoint((int) (lat * 1000000), (int)( lng * 1000000));
 			
 			overlay.p=p;
 			
@@ -118,5 +132,65 @@ public class DUBwiseMap extends MapActivity implements LocationListener {
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 	}
+
+	/* Creates the menu items */
+	public boolean onPrepareOptionsMenu(Menu menu) {
+
+		menu.clear();
+		
+		Bitmap kopter_icon = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getResources(),
+				R.drawable.icon),32,32,true);
+		
+	/*	MenuItem settings_menu=menu.add(0,MENU_SETTINGS,0,"Settings");
+		settings_menu.setIcon(android.R.drawable.ic_menu_preferences);
+		*/
+		
+		MenuItem freeze_menu=menu.add(0,MENU_ZOOM_KOPTER,0,"Zoom to UFO");
+		freeze_menu.setIcon(new BitmapDrawable(kopter_icon));
+	
+		if (overlay.p!=null) {
+			MenuItem freeze_menu2=menu.add(0,MENU_ZOOM_HOME,0,"Zoom to Home");
+			freeze_menu2.setIcon(R.drawable.rc);
+		}
+		
+		/*
+	    Menu features_menu=menu.addSubMenu("Features");
+	    features_menu.add(0, MENU_LEGEND, 0, "New Game").setCheckable(true);
+	    features_menu.add(0, MENU_GRID, 0, "Quit").setCheckable(true);
+	    */
+	    return true;
+	}
+
+	/* Handles item selections */
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		
+	    switch (item.getItemId()) {
+	    case MENU_ZOOM_KOPTER:
+	    	GeoPoint kopterPoint=new GeoPoint(MKProvider.getMK().gps_position.Latitude/10,MKProvider.getMK().gps_position.Longitude/10);
+			
+	    	mapView.getController().setCenter(kopterPoint);
+	    	mapView.getController().setZoom(14);
+	    	return true;
+	    case MENU_ZOOM_HOME:
+	    	mapView.getController().setCenter(overlay.p);
+	    	mapView.getController().setZoom(14);
+	    	
+	    	return true;
+	    }
+	    /*
+	    case MENU_FREEZE:
+	    	MKProvider.getMK().freeze_debug_buff=!MKProvider.getMK().freeze_debug_buff;
+	    	break;
+	    	
+	    case MENU_SETTINGS:
+	        //quit();
+	    	startActivity(new Intent(this, GraphSettingsActivity.class));
+	        return true;
+	    }
+	*/
+	    return false;
+	}
+
 
 }
