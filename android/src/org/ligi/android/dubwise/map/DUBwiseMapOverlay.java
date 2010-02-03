0d482dc27a144add05afb63da5a9a9eb8287646e
marcus bueschleb <ligi@ligi.de>
2010-02-03 17:56:42 +0100

    improve the marker painting

--- android/src/org/ligi/android/dubwise/map/DUBwiseMapOverlay.java
+++ android/src/org/ligi/android/dubwise/map/DUBwiseMapOverlay.java
@@ -16,11 +16,24 @@ import com.google.android.maps.MapView;
 class DUBwiseMapOverlay extends com.google.android.maps.Overlay {
 
 	DUBwiseMap context;
-	GeoPoint p;
+	GeoPoint p=null;
+	Paint circle_paint;
+	Bitmap home_icon;
+	Bitmap kopter_icon;
 	
 	public DUBwiseMapOverlay(DUBwiseMap context) {
-		p=new GeoPoint(0,0);
+		//	p=new GeoPoint(0,0);
 		this.context=context;
+		circle_paint=new Paint();
+		//paint.setColor(0x550000FF);
+		circle_paint.setColor(0x230000FF);
+		circle_paint.setStyle(Paint.Style.FILL);
+	
+		kopter_icon = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources(),
+				R.drawable.icon),42,42,true);
+		
+		home_icon = BitmapFactory.decodeResource(context.getResources(),
+				R.drawable.rc);
 	}
 	
 	@Override
@@ -30,35 +43,29 @@ class DUBwiseMapOverlay extends com.google.android.maps.Overlay {
 		super.draw(canvas, mapView, shadow);
 		Paint paint = new Paint();
 		// Converts lat/lng-Point to OUR coordinates on the screen.
+		float gps_radius_in_pixels=mapView.getProjection().metersToEquatorPixels(500.0f);
 		
-		Point myScreenCoords = new Point();
-		mapView.getProjection().toPixels(p, myScreenCoords);
+		if (p!=null) {
+			Point myScreenCoords = new Point();
+			mapView.getProjection().toPixels(p, myScreenCoords);
+			canvas.drawCircle(myScreenCoords.x, myScreenCoords.y, gps_radius_in_pixels, circle_paint);
+			canvas.drawBitmap(home_icon, myScreenCoords.x-home_icon.getWidth()/2, myScreenCoords.y-home_icon.getHeight()/2, paint);
+			canvas.drawText("lat" + p.getLatitudeE6() + " lon" + p.getLongitudeE6() , (float)myScreenCoords.x,(float)myScreenCoords.y,paint);
+			
+		}
+		
 		paint.setStrokeWidth(1);
 		paint.setARGB(255, 255, 255, 255);
 		paint.setStyle(Paint.Style.STROKE);
-		
 		
 		GeoPoint kopterPoint=new GeoPoint(MKProvider.getMK().gps_position.Latitude/10,MKProvider.getMK().gps_position.Longitude/10);
 		Point kopterScreenCoords = new Point();
 		mapView.getProjection().toPixels(kopterPoint, kopterScreenCoords);
 		
-		
-		Bitmap kopter_icon = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources(),
-				R.drawable.icon),32,32,true);
-	
-		
-		
-		Bitmap home_icon = BitmapFactory.decodeResource(context.getResources(),
-				R.drawable.rc);
-	
-		
-		canvas.drawBitmap(home_icon, myScreenCoords.x-home_icon.getWidth()/2, myScreenCoords.y-home_icon.getHeight()/2, paint);
+		canvas.drawCircle(kopterScreenCoords.x, kopterScreenCoords.y, gps_radius_in_pixels, circle_paint);
 		canvas.drawBitmap(kopter_icon, kopterScreenCoords.x-kopter_icon.getWidth()/2, kopterScreenCoords.y-kopter_icon.getHeight()/2, paint);
-		/*canvas.drawText("Here I am...", myScreenCoords.x, myScreenCoords.y,
-				paint);
-		*/
-		return true;
 
+		return true;
 	}
 
 }
