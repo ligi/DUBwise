cc7e024c4cd3e1c98347bbe1400951f89d6df3e5
marcus bueschleb <ligi@ligi.de>
2010-02-04 02:02:00 +0100

    removing dups

--- android/shared_src/org/ligi/ufo/MKParamDefinitions.java
+++ android/shared_src/org/ligi/ufo/MKParamDefinitions.java
@@ -3,15 +3,7 @@ package org.ligi.ufo;
 public interface MKParamDefinitions
 {
 
-    public final static int PARAMTYPE_BYTE=0;      // normal byte
-    public final static int PARAMTYPE_MKBYTE=1;    // has potis @ end
-    public final static int PARAMTYPE_BITSWITCH=2; // a bit aka boolean
-    public final static int PARAMTYPE_STICK=3;     // a stick ( 1-12 )
-    public final static int PARAMTYPE_KEY=4;       // a key
-    public final static int PARAMTYPE_BITMASK=5;   // a bitmask ( byte )
-
-    public final static int PARAMTYPE_CHOICE=6;
-
+ 
     //  choice must be last!
 
 }
