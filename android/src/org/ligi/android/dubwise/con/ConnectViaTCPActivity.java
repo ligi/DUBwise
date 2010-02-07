a620fe1a82f1d1db5fc598ea6df4aa12cdd9e3cd
marcus bueschleb <ligi@ligi.de>
2010-02-07 16:02:07 +0100

    add username and pwd fields needed for QMK Protocol

--- android/src/org/ligi/android/dubwise/con/ConnectViaTCPActivity.java
+++ android/src/org/ligi/android/dubwise/con/ConnectViaTCPActivity.java
@@ -10,20 +10,26 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TableRow;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import android.app.Activity;
 
 import android.content.SharedPreferences;
 
-public class ConnectViaTCPActivity extends Activity implements OnClickListener {
+public class ConnectViaTCPActivity extends Activity implements OnClickListener, OnCheckedChangeListener {
 
 	// public MapView map;
 	/** Called when the activity is first created. */
     
     EditText port_text;
     EditText host_text;
+    EditText user_text;
+    EditText pwd_text;
+    
     CheckBox qmk_check;
     
 	@Override
@@ -36,15 +42,24 @@ public class ConnectViaTCPActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.connect_tcp);
 
 		port_text=((EditText) findViewById(R.id.PortEditText));
-		port_text.setText(settings.getInt("tcp-port", 64400) );
+		port_text.setText(""+settings.getInt("tcp-port", 64400) );
 		
+		user_text=((EditText) findViewById(R.id.UserNameEditText));
+		user_text.setText(settings.getString("qmk-user", "anonymous") );
+		
+		pwd_text=((EditText) findViewById(R.id.PasswordEditText));
+		pwd_text.setText(settings.getString("qmk-pwd", "") );
+				
 		host_text=((EditText) findViewById(R.id.HostEditText));
 		host_text.setText(settings.getString("tcp-host", "127.0.0.1") );
 		
 		qmk_check=((CheckBox) findViewById(R.id.QMKCheckBox));
 		qmk_check.setChecked(settings.getBoolean("tcp-qmk", true));
 		
+		qmk_check.setOnCheckedChangeListener(this);
 		((Button) findViewById(R.id.ConnectButton)).setOnClickListener(this );
+		
+		updateQMKVisibility(qmk_check.isChecked());
 		
 	}
 	
@@ -85,6 +100,19 @@ public class ConnectViaTCPActivity extends Activity implements OnClickListener {
 	protected void onDestroy() {
 		super.onDestroy();
 		ActivityCalls.onDestroy(this);
+	}
+
+	public void updateQMKVisibility(boolean visible)
+	{
+		((TableRow)findViewById(R.id.UserNameTableRow)).setVisibility(visible?View.VISIBLE:View.GONE);
+		((TableRow)findViewById(R.id.PasswordTableRow)).setVisibility(visible?View.VISIBLE:View.GONE);
+
+	}
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		if (buttonView==qmk_check)
+			updateQMKVisibility(isChecked);
+	
 	}
 
 }
