1e7c5aab464c415071411ba56875b19defc6f615
marcus bueschleb <ligi@ligi.de>
2010-04-02 02:04:25 +0200

    speak some more

--- android/src/org/ligi/android/dubwise/voice/StatusVoice.java
+++ android/src/org/ligi/android/dubwise/voice/StatusVoice.java
@@ -42,14 +42,29 @@ public class StatusVoice implements OnInitListener, Runnable,
 	private boolean init_done = false;
 	private HashMap<String, String> voice_params;
 
+	private boolean init_started=false;
+	
+
+	private MKCommunicator mk = MKProvider.getMK();
+	private int last_nc_flags=-1;
+	private int last_fc_flags=-1;
+	
+	
 	public void init(Activity activity) {
+		if (isInitStarted()) // don't do it again
+			return;
+		init_started=true;
 		VoicePrefs.init(activity);
 		mTts = new TextToSpeech(activity, this);
-
+	}
+	
+	public boolean isInitStarted() {
+		return init_started;
 	}
 
 	@Override
 	public void onInit(int arg0) {
+		
 		mTts.setLanguage(Locale.US);
 
 		voice_params = new HashMap<String, String>();
@@ -70,69 +85,113 @@ public class StatusVoice implements OnInitListener, Runnable,
 
 		return thisRef;
 	}
-
-	MKCommunicator mk = MKProvider.getMK();
+	
+	private boolean told_rclost=false;
 
 	@Override
 	public void run() {
-
+		int sleep = 10;
+		int pause_timeout=0;
+		
 		while (true) {
 
 			// Log.i("DUBwise","mk"+mk.ready() + "  init"+ init_done);
-			int sleep = 10;
+			
 			if (VoicePrefs.isVoiceEnabled()&&mk.ready() && init_done) {
-				Log.i("DUBwise", "will_speak" + mTts.isSpeaking());
+				
+			if (!mTts.isSpeaking()) { // no breaking news
+				String what2speak="";
+				Log.i("DUBwise","not speaking");
+				// breaking news section
+				if ((!last_version_str.equals("" + mk.version.major + "."
+						+ mk.version.minor + " "
+						+ (char) ('A' + mk.version.patch)))
+						&& (VoicePrefs.isConnectionInfoEnabled())) 
+					{
 
-				// TODO do breaking news
-				if (false)// (!mTts.isSpeaking())
-				{
-
-					mTts.stop();
-
-					// isSpeaking() returns true too early - waiting a bit
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
+					last_version_str = "" + mk.version.major + "."
+							+ mk.version.minor + " "
+							+ (char) ('A' + mk.version.patch);
+					if (mk.is_mk())
+						mTts.speak("Connected to Flight Control Version "
+								+ last_version_str, TextToSpeech.QUEUE_ADD,
+								voice_params);
+					else if (mk.is_navi())
+						mTts.speak("Connected to Navigation Control Version "
+								+ last_version_str, TextToSpeech.QUEUE_ADD,
+								voice_params);
+					else if (mk.is_mk3mag())
+						mTts.speak("Connected to Mikrokopter Compas Version "
+								+ last_version_str, TextToSpeech.QUEUE_ADD,
+								voice_params);
+					else if (mk.is_fake())
+						mTts.speak(
+								"Connected to a Simulated connection Version "
+										+ last_version_str,
+								TextToSpeech.QUEUE_ADD, voice_params);
+					else
+						mTts.speak("Connected to the unknown "
+								+ last_version_str, TextToSpeech.QUEUE_ADD,
+								voice_params);
 					}
+				
+				else if (mk.is_navi()&&mk.gps_position.NCFlags!=last_nc_flags)
+				{
+					last_nc_flags=mk.gps_position.NCFlags;
+
+
+					if (mk.gps_position.isFreeModeEnabled())
+						mTts.speak(" Free Mode Enabled. ", TextToSpeech.QUEUE_ADD,
+							voice_params);
+					
+					if (mk.gps_position.isPositionHoldEnabled())
+						mTts.speak(" Position Hold Enabled. ", TextToSpeech.QUEUE_ADD,
+							voice_params);
+
+					if (mk.gps_position.isComingHomeEnabled())
+						mTts.speak(" Coming Home Enabled ", TextToSpeech.QUEUE_ADD,
+							voice_params);
+					
+
+					if (mk.gps_position.isTargetReached())
+						mTts.speak(" Target Reached. ", TextToSpeech.QUEUE_ADD,
+							voice_params);
+					
+
+					if (mk.gps_position.isManualControlEnabled())
+						mTts.speak(" Manual Control Enabled. ", TextToSpeech.QUEUE_ADD,
+							voice_params);
+
+					if (mk.gps_position.isRangeLimitReached())
+						mTts.speak(" Range Limit Reached. ", TextToSpeech.QUEUE_ADD,
+							voice_params);
+
+				}
+				else if (mk.is_navi()&&mk.gps_position.FCFlags!=last_fc_flags)
+				{
+					last_fc_flags=mk.gps_position.FCFlags;
+
+					if (mk.gps_position.areEnginesOn())
+						what2speak+=" Engines are on ";
+					else
+						what2speak+=" Engines are off ";
+					
+					if (mk.gps_position.isFlying())
+						what2speak+=" UFO is flying ";
+					
+
+					if (mk.gps_position.isCalibrating())
+						what2speak+=" UFO is Calibrating ";
+					
+					mTts.speak(what2speak, TextToSpeech.QUEUE_ADD,
+							voice_params);
 
 				}
 
-				if (!mTts.isSpeaking()) {
-
-					if ((!last_version_str.equals("" + mk.version.major + "."
-							+ mk.version.minor + " "
-							+ (char) ('A' + mk.version.patch)))
-							&& (VoicePrefs.isConnectionInfoEnabled())) {
-						last_version_str = "" + mk.version.major + "."
-								+ mk.version.minor + " "
-								+ (char) ('A' + mk.version.patch);
-						if (mk.is_mk())
-							mTts.speak("Connected to Flight Control Version "
-									+ last_version_str,
-									TextToSpeech.QUEUE_FLUSH, voice_params);
-						else if (mk.is_navi())
-							mTts.speak(
-									"Connected to Navigation Control Version "
-											+ last_version_str,
-									TextToSpeech.QUEUE_FLUSH, voice_params);
-						else if (mk.is_mk3mag())
-							mTts.speak(
-									"Connected to Mikrokopter Compas Version "
-											+ last_version_str,
-									TextToSpeech.QUEUE_FLUSH, voice_params);
-						else if (mk.is_fake())
-							mTts.speak(
-									"Connected to a Simulated connection Version "
-											+ last_version_str,
-									TextToSpeech.QUEUE_FLUSH, voice_params);
-						else
-							mTts.speak("Connected to the unknown "
-									+ last_version_str,
-									TextToSpeech.QUEUE_FLUSH, voice_params);
-						
-					}
-
-					switch ((play_pos++) % 8) {
+				else if ((pause_timeout--)<0)
+				{
+					
+					switch ((play_pos++) % 9) {
 					case 0:
 						if (mk.is_navi() && mk.hasNaviError()
 								&& VoicePrefs.isVoiceNaviErrorEnabled()) {
@@ -204,22 +263,40 @@ public class StatusVoice implements OnInitListener, Runnable,
 						break;
 
 					case 6:
+						if ((mk.stats.flying_time()>0)
+								&& (VoicePrefs.isFlightTimeEnabled())) {
+									String str2speak="Flight time";
+									if ((mk.stats.flying_time()/60)!=0)
+										str2speak+=" " + mk.stats.flying_time()/60 + " minutes ";
+									
+									if ((mk.stats.flying_time()%60)!=0)
+										str2speak+=" " + mk.stats.flying_time()%60 + " seconds. ";
+									
+									mTts.speak(str2speak, TextToSpeech.QUEUE_ADD,voice_params);
+
+						}
+						break;
+
+						
+						
+						
+						
+					case 7:
 						if ((mk.is_navi() || mk.is_fake())
 								&& (VoicePrefs.isVoiceSatelitesEnabled())) {
-							mTts.speak("" + mk.SatsInUse()
+						Log.i("DUBwise synth" , "!!!!!!!!!1text result" +	mTts.speak("" + mk.SatsInUse()
 									+ " Satelites are used for GPS.",
-									TextToSpeech.QUEUE_ADD, voice_params);
+									TextToSpeech.QUEUE_ADD, voice_params));
 
 						}
 
 						break;
 
-					case 7:
-						mTts.playSilence(5000, TextToSpeech.QUEUE_ADD,
-								voice_params);
-
-
-					}
+					case 8:
+						pause_timeout=VoicePrefs.getPauseTime()/sleep;
+						break;
+					} // switch
+				}
 
 				}
 			}
