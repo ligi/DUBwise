8ce327577c35e2716f320e0e2ac0d7119a9438f4
marcus bueschleb <ligi@ligi.de>
2010-04-23 03:34:35 +0200

    rm typo

--- android/src/org/ligi/android/dubwise/DUBwise.java
+++ android/src/org/ligi/android/dubwise/DUBwise.java
@@ -13,7 +13,7 @@
  *  Additionally to the Creative Commons terms it is not allowed
  *  to use this project in _any_ violent manner! 
  *  This explicitly includes that lethal Weapon owning "People" and 
- *  Organisations (e.g. Army & Police) 
+ *  Organizations (e.g. Army & Police) 
  *  are not allowed to use this Project!
  *
  **************************************************************************/
