24e7de780678ac8c12bac7b9d12241c6df1b6560
marcus bueschleb <ligi@ligi.de>
2010-05-15 18:42:56 +0200

    some doc

--- android/src/org/ligi/android/dubwise/map/WayPoint.java
+++ android/src/org/ligi/android/dubwise/map/WayPoint.java
@@ -1,6 +1,32 @@
+/****************************************************
+ *                                            
+ * Author:        Marcus -LiGi- Bueschleb     
+ * 
+ * see README for further Infos
+ *
+ * License:
+ *  http://creativecommons.org/licenses/by-nc-sa/2.0/de/ 
+ *  (Creative Commons / Non Commercial / Share Alike)
+ *  Additionally to the Creative Commons terms it is not allowed
+ *  to use this project in _any_ violent manner! 
+ *  This explicitly includes that lethal Weapon owning "People" and 
+ *  Organisations (e.g. Army & Police) 
+ *  are not allowed to use this Project!
+ *
+ *
+ *
+ *****************************************************/
+
 package org.ligi.android.dubwise.map;
 
 import com.google.android.maps.GeoPoint;
+
+/**
+ * class representing a waypoint 
+ * 
+ * @author Marcus -ligi- Bueschleb
+ *
+ */
 
 public class WayPoint {
 
