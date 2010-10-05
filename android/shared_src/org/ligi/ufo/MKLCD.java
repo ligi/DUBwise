d0963fe5a2065a70c97af52f35524208a193a7ff
marcus bueschleb <ligi@ligi.de>
2010-10-05 12:30:49 +0200

    code style/doc

--- android/shared_src/org/ligi/ufo/MKLCD.java
+++ android/shared_src/org/ligi/ufo/MKLCD.java
@@ -1,9 +1,8 @@
 /***********************************************************************
- *                                          
- * Handling of MK LCD                       
- *                                          
- * Author:        Marcus -LiGi- Bueschleb   
- * 
+ * DUBwise
+ * by Marcus -Ligi- Bueschleb 
+ * http://ligi.de
+ *                               
  * License:
  *  http://creativecommons.org/licenses/by-nc-sa/2.0/de/ 
  *  (Creative Commons / Non Commercial / Share Alike)
@@ -15,88 +14,107 @@
  *
  *
  *************************************************************************/
-
-
 package org.ligi.ufo;
 
-public class MKLCD
-    //   implements Runnable
-{
+/**
+ * handle the LCD from the MK 
+ * the LCD is a 4x20 char text 
+ * which is divided into pages 
+ * provided by FC/NC/MK3MAG
+ *   
+ * @author Marcus -ligi- Bueschleb http://ligi.de
+ *
+ */
+public class MKLCD {
 
-    MKCommunicator mk=null;
+	private MKCommunicator mk=null;
 
-    int last_recieved_page=0;
-    int act_page=0;
-    public int pages=-1;
-
-    private String[] lcd_buf;
-    private String[] lcd_buf_;
-
-    public String[] get_act_page()
-    { return lcd_buf; }
-
-    public MKLCD(MKCommunicator _mk)
-    {
-	lcd_buf=new String[4];
-
-	lcd_buf[0]="no                  ";
-	lcd_buf[1]="DisplayData         " ;
-	lcd_buf[2]="read                ";
-	lcd_buf[3]="yet                 ";
-	mk=_mk;
+	private int last_recieved_page=0;
+	private int act_page=0;
+	private int pages=-1;
+    private String[] lcd_buf, lcd_backbuf;
+    
+    public String[] get_act_page() { 
+    	return lcd_buf; 
     }
-
-    public void set_page(int page)
-    {
-	act_page=page;
+    
+    /**
+     * @return the count of LCD-Pages provided
+     */
+    public int getPageCount() {
+    	return pages;
     }
-
-    public void handle_lcd_data(int[] data)
-    {
-
-	lcd_buf_=new String[4];
-	last_recieved_page=data[0];
-	pages=data[1];
-
-
-
-	for(int row=0;row<4;row++)
-	    {
-		lcd_buf_[row]="";
-		for(int col=0;col<20;col++)
-		    try {		   
-
-			lcd_buf_[row]+=(char)data[row*20+col+2];
-		    }
-			catch (Exception e) {		lcd_buf_[row]+=" "; }
-	    }
+    
+    /**
+     * @return the last page which was recieved
+     */
+    public int getLastRecievedPage() {
+    	return last_recieved_page;
+    }
+    
+    public MKLCD(MKCommunicator _mk) {
+		lcd_buf=new String[4];
 	
-
-	lcd_buf=lcd_buf_;
+		lcd_buf[0]="no                  ";
+		lcd_buf[1]="DisplayData         ";
+		lcd_buf[2]="read                ";
+		lcd_buf[3]="yet                 ";
+		mk=_mk;
     }
 
-    public void trigger_LCD()
-    {
-	mk.trigger_LCD_by_page(act_page);
+    /**
+     * set the actual page
+     * 
+     * @param page - the new actual page
+     */
+    public void set_page(int page) {
+    	act_page=page;
     }
 
-    
+    public void handle_lcd_data(int[] data) {
 
-    public void LCD_NEXTPAGE()
-    {
-	if (act_page<pages)
-	    act_page++;
-	else
-	    act_page=0;
-
+		lcd_backbuf=new String[4];
+		last_recieved_page=data[0];
+		pages=data[1];
+	
+		for(int row=0;row<4;row++) {
+			lcd_backbuf[row]="";
+			for(int col=0;col<20;col++)
+			    try {		   
+			    	lcd_backbuf[row]+=(char)data[row*20+col+2];
+			    }
+				catch (Exception e) {
+					lcd_backbuf[row]+=" "; 
+				}
+		    }
+		
+		lcd_buf=lcd_backbuf;
     }
 
-    public void LCD_PREVPAGE()
-    {
-	if (act_page>0)
-	    act_page--;
-	else
-	    act_page=pages;
+    /**
+     * request the LCD Data for the act page
+     */
+    public void trigger_LCD() {
+    	mk.trigger_LCD_by_page(act_page);
     }
-    
+
+    /**
+     * jump to previous LCD page
+     */
+    public void LCD_NEXTPAGE()  {
+		if (act_page<pages)
+		    act_page++;
+		else
+		    act_page=0;
+    }
+
+    /**
+     * jump to next LCD page
+     */
+    public void LCD_PREVPAGE() {
+		if (act_page>0)
+		    act_page--;
+		else
+		    act_page=pages;
+	}
 }
