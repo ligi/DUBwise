1ad1e55267711e21d18210f3335034ee72f8fd0d
marcus bueschleb <ligi@ligi.de>
2010-10-06 13:09:03 +0200

    adding file with MK Protocol Definitions like slave addrs

---
+++ android/shared_src/org/ligi/ufo/MKProtocolDefinitions.java
@@ -0,0 +1,17 @@
+package org.ligi.ufo;
+
+public class MKProtocolDefinitions {
+
+    public final static byte FC_SLAVE_ADDR              = 1;
+    public final static byte NAVI_SLAVE_ADDR            = 2;
+    public final static byte MK3MAG_SLAVE_ADDR          = 3;
+    public final static byte FOLLOWME_SLAVE_ADDR        = 10;
+
+    public final static byte RIDDIM_SLAVE_ADDR          = 12;
+    public final static byte RE_SLAVE_ADDR              = 23;
+
+    
+    public final static byte FAKE_SLAVE_ADDR              = 42;
+
+    
+}
