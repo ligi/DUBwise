c7b2e6f4e7c24f341fb6895a30f9de1d3a4ba9f0
marcus bueschleb <ligi@ligi.de>
2010-10-29 14:08:51 +0200

    rm nl

--- android/shared_src/org/ligi/ufo/DUBwiseNotificationListenerInterface.java
+++ android/shared_src/org/ligi/ufo/DUBwiseNotificationListenerInterface.java
@@ -24,7 +24,6 @@ public interface DUBwiseNotificationListenerInterface {
 
 	public final static byte NOTIFY_CONNECTION_CHANGED=1;
 	public final static byte NOTIFY_DISCONNECT=2;
-	
-	
+		
 	public void processNotification(byte notification);
 }
