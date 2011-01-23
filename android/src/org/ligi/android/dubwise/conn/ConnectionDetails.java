457af036da246002218972eab32e7af874eabe84
marcus bueschleb <ligi@ligi.de>
2011-01-23 23:44:30 +0100

    simplify code by using RefreshingStringListActivity

--- android/src/org/ligi/android/dubwise/conn/ConnectionDetails.java
+++ android/src/org/ligi/android/dubwise/conn/ConnectionDetails.java
@@ -21,95 +21,56 @@
 package org.ligi.android.dubwise.conn;
 
 import org.ligi.android.dubwise.conn.bluetooth.BluetoothCommunicationAdapter;
-import org.ligi.android.dubwise.helper.ActivityCalls;
-import org.ligi.android.dubwise.helper.DUBwiseBaseListActivity;
+import org.ligi.android.dubwise.helper.RefreshingStringListActivity;
 import org.ligi.ufo.MKCommunicator;
-import android.os.Bundle;
-import android.os.Handler;
-import android.widget.ArrayAdapter;
 
-public class ConnectionDetails extends DUBwiseBaseListActivity implements Runnable{
+public class ConnectionDetails extends RefreshingStringListActivity {
 
-	private String[] menu_items;
-	private ArrayAdapter<String> adapter;
-	private boolean running=true;
-	private final static int VALUE_COUNT=15;
-	
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		menu_items=new String[VALUE_COUNT];
-		refresh_values();
-		
-		ActivityCalls.beforeContent(this);
-		adapter=new ArrayAdapter<String>(this,
-	            android.R.layout.simple_list_item_1, menu_items);
-		
-	    this.setListAdapter(adapter);
-	       new Thread(this).start();
-	}
-
-	
-	private void refresh_values() {
-		MKCommunicator mk=MKProvider.getMK();
-		int pos=0;
-		
-		menu_items[pos++]="Time: " + mk.getConnectionTime() + " s";
-		menu_items[pos++]="Bytes in: " + mk.stats.bytes_in;
-		menu_items[pos++]="Bytes out: " + mk.stats.bytes_out;
-		menu_items[pos++]="CRC Fails: " + mk.stats.crc_fail;
-		menu_items[pos++]="Debug Data: " + mk.stats.debug_data_count + "/" + mk.stats.debug_data_request_count;
-		menu_items[pos++]="Analog Names: " + mk.stats.debug_names_count + "/" + mk.stats.debug_name_request_count;
-		menu_items[pos++]="LCD Data: " + mk.stats.lcd_data_count + "/" + mk.stats.lcd_data_request_count;
-		menu_items[pos++]="ExternalControl: " + mk.stats.external_control_confirm_frame_count + "/" + mk.stats.external_control_request_count;
-		menu_items[pos++]="Version: " + mk.stats.version_data_count +"/" + mk.stats.version_data_request_count;
-		menu_items[pos++]="Params: " + mk.stats.params_data_count +"/" + mk.stats.params_data_request_count;
-		menu_items[pos++]="OSD: " + mk.stats.navi_data_count ;
-		menu_items[pos++]="Angles: " + mk.stats.angle_data_count;
-		menu_items[pos++]="Motortest: " + mk.stats.motortest_request_count;
-		menu_items[pos++]="Other: " + mk.stats.other_data_count;
-		
-		
-
-		if (mk.getCommunicationAdapter() instanceof BluetoothCommunicationAdapter)
-			menu_items[pos++]="BTRssi" + ((BluetoothCommunicationAdapter)(mk.getCommunicationAdapter())).getRSSI();
-		else
-			menu_items[pos++]="NO BT RSSI" ;
-		
-	}
-		
-	@Override
-	protected void onResume() {
-		super.onResume();
-		ActivityCalls.afterContent(this);
+	public String getStringByPosition(int pos) {
+    	MKCommunicator mk=MKProvider.getMK();
+    	
+		switch(pos) {
+			case 0: 
+				return "Time: " + mk.getConnectionTime() + " s";
+			case 1:
+				return "Bytes in:" + mk.stats.bytes_in;
+			case 2:
+				return "Bytes in:" + mk.stats.bytes_out;
+			case 3:
+        		return "Debug Data: " + mk.stats.debug_data_count + "/" + mk.stats.debug_data_request_count;            			
+        	case 4:
+        		return "Stick: " + mk.stats.stick_data_count + "/" + mk.stats.stick_data_request_count;
+        	case 5:            		
+        		return "Analog Names: " + mk.stats.debug_names_count + "/" + mk.stats.debug_name_request_count;
+        	case 6:            		
+        		return "LCD Data: " + mk.stats.lcd_data_count + "/" + mk.stats.lcd_data_request_count;
+        	case 7:            		
+        		return "ExternalControl: " + mk.stats.external_control_confirm_frame_count + "/" + mk.stats.external_control_request_count;
+        	case 8:            		
+        		return "Version: " + mk.stats.version_data_count +"/" + mk.stats.version_data_request_count;
+        	case 9:            		
+        		return "OSD: " + mk.stats.navi_data_count;
+        	case 10:            		
+            	return "Angles: " + mk.stats.angle_data_count;
+        	case 11:            		
+        		return "Motortest: " + mk.stats.motortest_request_count;
+        	case 12:            		
+        		return "Params: " + mk.stats.params_data_count +"/" + mk.stats.params_data_request_count;
+        	case 13:
+        		return "Other: " + mk.stats.other_data_count;
+        	case 14:
+        		if (mk.getCommunicationAdapter() instanceof BluetoothCommunicationAdapter)
+        			return "BTRssi" + ((BluetoothCommunicationAdapter)(mk.getCommunicationAdapter())).getRSSI();
+        		else
+        			return "NO BT RSSI" ;
+    		
+        }
+		return null;
 	}
 
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		running=false;
-	}
-	 final Handler mHandler = new Handler();
-	    
-	 	// Create runnable for posting
-	   final Runnable mUpdateResults = new Runnable() {
-	       public void run() {
-	    	   adapter.notifyDataSetChanged();
-	       }
-	    };
-	@Override
-	public void run() {
-		while (running) {
-			refresh_values();
-			mHandler.post(mUpdateResults);
-			try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		}
+	public int getRefreshSleep() {
+		return 100;
 	}
 
 }
