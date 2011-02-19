2912c0da2abf96255576feea622bdc8d32c13a33
marcus bueschleb <ligi@ligi.de>
2011-02-19 06:35:19 +0100

    rm unused import which makes problems with j2me + javadoc

--- android/shared_src/org/ligi/ufo/WayPoint.java
+++ android/shared_src/org/ligi/ufo/WayPoint.java
@@ -1,10 +1,26 @@
+/*********************************************
+ * 
+ * License:
+ *  http://creativecommons.org/licenses/by-nc-sa/2.0/de/ 
+ *  (Creative Commons / Non Commercial / Share Alike)
+ *  Additionally to the Creative Commons terms it is not allowed
+ *  to use this project in _any_ violent manner! 
+ *  This explicitly includes that lethal Weapon owning "People" and 
+ *  Organisations (e.g. Army & Police) 
+ *  are not allowed to use this Project!
+ *
+ ********************************************/
+
 package org.ligi.ufo;
 
-import com.google.android.maps.GeoPoint;
-
+/**
+ * class to describe a MikroKopter WayPoint
+ * 
+ * @author ligi ( aka: Marcus Bueschleb | mail: ligi at ligi dot de )
+ *
+ */
 public class WayPoint {
 
-	
 	private int hold_time=23;        // in s
 	private int tolerance_radius=5;  // in m
 	private int channel_event=200;   // 
