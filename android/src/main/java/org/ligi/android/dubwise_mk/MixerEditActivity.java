e2a8c961dd482cc0bf7a7dcbcd99df3f4b73db54
ligi <ligi@ligi.de>
2013-08-22 23:40:49 +0200

    cleanup

--- android/src/main/java/org/ligi/android/dubwise_mk/MixerEditActivity.java
+++ android/src/main/java/org/ligi/android/dubwise_mk/MixerEditActivity.java
@@ -1,11 +1,7 @@
 /**************************************************************************
  *
- * Activity to edit the Mixer Table
- *
- * Author:  Marcus -LiGi- Bueschleb   
- *
  * Project URL:
- *  http://mikrokopter.de/ucwiki/en/DUBwise
+ *  http://github.com/ligi/DUBwise
  *
  * License:
  *  http://creativecommons.org/licenses/by-nc-sa/2.0/de/ 
@@ -27,15 +23,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
-import android.text.TextWatcher;
 import android.text.method.KeyListener;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
@@ -50,35 +42,20 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
 import org.ligi.android.dubwise_mk.conn.MKProvider;
-import org.ligi.tracedroid.logging.Log;
 import org.ligi.ufo.MixerManager;
 
-public class MixerEditActivity extends BaseActivity implements OnItemSelectedListener, OnCheckedChangeListener, TextWatcher, OnEditorActionListener, KeyListener {
+/**
+ * Activity to edit the Mixer Table
+ */
+public class MixerEditActivity extends BaseActivity implements OnCheckedChangeListener, OnEditorActionListener, KeyListener {
 
-    String[] menu_items;
-
-    // 	each is tagged with the id of the row
-    Spinner spinners[];
     EditText edit_texts[][];
-    CheckBox checkboxes[];
-    int act_topic;
     EditText name_edit;
     TableLayout table;
 
-    // public MapView map;
-
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-/*
-        menu_items=new String[MKProvider.getMK().params.tab_stringids.length];
-		for (int i=0;i<menu_items.length;i++)
-		menu_items[i]=getString(DUBwiseStringHelper.table[MKProvider.getMK().params.tab_stringids[i]]);
-		*/
 
         ScrollView view = new ScrollView(this);
 
@@ -89,7 +66,6 @@ public class MixerEditActivity extends BaseActivity implements OnItemSelectedLis
         table.setLayoutParams(lp);
 
         //table.setColumnStretchable(1, true);
-
 
         TableRow name_row = new TableRow(this);
 
@@ -141,125 +117,6 @@ public class MixerEditActivity extends BaseActivity implements OnItemSelectedLis
             table.addView(new_row);
         }
         copyMixerManagerValues2Layout();
-        /*
-        for (int i=0;i<menu_items.length;i++)
-        {
-        	TableRow row=new TableRow(this);
-        	table.addView(row);
-			row.setPadding(0, 5, 0, 5);
-			TextView text_v=new TextView(this);
-        	text_v.setText(DUBwiseStringHelper.table[MKProvider.getMK().params.field_stringids[act_topic][i]]);
-        	text_v.setMinHeight(50);
-        	text_v.setPadding(3, 0, 5, 0);
-        	row.addView(text_v);
-        
-        	//text_v.addTextChangedListener(this);
-        	//text_v.setOnEditorActionListener(this);
-        	row.setTag(i);
-        	MKParamsParser params=MKProvider.getMK().params;
-        	switch(MKProvider.getMK().params.field_types[act_topic][i]) {
-
-        	case MKParamDefinitions.PARAMTYPE_STICK:
-        		Spinner spinner=new Spinner(this);
-        		String stick_strings[]=new String[12];
-        		
-        		for (int stick=0;stick<12;stick++)
-        			stick_strings[stick]=""+stick;
-
-         		ArrayAdapter<String> spinner_adapter = new ArrayAdapter<String>(this,
-        	            android.R.layout.simple_spinner_item , stick_strings);
-         		spinner.setAdapter(spinner_adapter);
-        		
-        		row.addView(spinner);
-        		break;
-
-        	case MKParamDefinitions.PARAMTYPE_BITSWITCH:
-        	
-        		checkboxes[i]=new CheckBox(this);
-        		checkboxes[i].setTag(i);
-        		checkboxes[i].setOnCheckedChangeListener(this);
-        		checkboxes[i].setChecked(!((MKProvider.getMK().params.get_field_from_act(MKProvider.getMK().params.field_positions[act_topic][i]/8)&(1<<MKProvider.getMK().params.field_positions[act_topic][i]%8))==0));
-        		row.addView(checkboxes[i]);
-        		break;
-
-        	case MKParamDefinitions.PARAMTYPE_BITMASK:
-        		row.addView(new EditText(this));
-        		LinearLayout lin=new LinearLayout(this);
-        		for (int bm=0;bm<8;bm++)
-        			lin.addView(new CheckBox(this));
-        		TableRow new_row=new TableRow(this);
-        		TableRow.LayoutParams layout_p=(new TableRow.LayoutParams());
-        		layout_p.span=3;
-        		lin.setLayoutParams(layout_p);
-        		new_row.addView(lin);
-        		table.addView(new_row);
-        		new_row.setVisibility(View.GONE);
-        		break;
-
-        		
-        	case MKParamDefinitions.PARAMTYPE_MKBYTE:
-        		edit_texts[i]=new EditText(this);
-        		edit_texts[i].setText("" +params.field[params.act_paramset][params.field_positions[act_topic][i]] );
-        		
-        		edit_texts[i].setTag(new Integer(i));
-        		edit_texts[i].setInputType(InputType.TYPE_CLASS_NUMBER);
-//        		edit_texts[i].addTextChangedListener(this);
-        		//edit_texts[i].setKeyListener(this);
-        		final int act_i=i;
-        		
-        		edit_texts[i].addTextChangedListener(new TextWatcher() {
-
-					@Override
-					public void afterTextChanged(Editable s) {
-						try{
-							int txt_val=Integer.parseInt(s.toString());
-							if (txt_val>255)
-								txt_val=255;
-							else if (txt_val<0)
-								txt_val=0;
-							MKProvider.getMK().params.field[MKProvider.getMK().params.act_paramset][MKProvider.getMK().params.field_positions[act_topic][act_i]]=txt_val;						// TODO Auto-generated method stub
-							Log.d("!!!" , "" + txt_val);
-							
-							if (!(""+txt_val).equals(edit_texts[act_i].getText().toString()))
-								edit_texts[act_i].setText( "" + txt_val);
-						}catch(Exception e){};
-							
-					}
-
-					@Override
-					public void beforeTextChanged(CharSequence s, int start,
-							int count, int after) {
-									}
-
-					@Override
-					public void onTextChanged(CharSequence s, int start,
-							int before, int count) {
-						
-					}});
-        		//edit_texts[i].set
-        		
-        		spinners[i]=new Spinner(this);
-        		String poti_strings[]=new String[6];
-        		poti_strings[0]="no Poti";
-        		for (int s=1;s<6;s++) 
-        			poti_strings[s]="Poti " + s;
-        		
-         		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-        	            android.R.layout.simple_spinner_item , poti_strings);
-         		spinners[i].setAdapter(adapter);
-        		spinners[i].setTag(new Integer(i));
-         		
-        	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        	    
-        	    spinners[i].setOnItemSelectedListener(this);
-        	    
-        		row.addView(edit_texts[i]);
-        		row.addView(spinners[i]);
-        		break;
-        	}
-        	
-        }
-        */
 
         view.addView(table);
         this.setContentView(view);
@@ -272,8 +129,9 @@ public class MixerEditActivity extends BaseActivity implements OnItemSelectedLis
         for (byte type = 0; type < 4; type++) {
             byte[] data = MKProvider.getMK().mixer_manager.getValuesByType(type);
 
-            for (int row = 0; row < 12; row++)
-                edit_texts[row][type].setText("" + data[row]);
+            for (int row = 0; row < 12; row++) {
+                edit_texts[row][type].setText(String.valueOf(data[row]));
+            }
         }
 
         name_edit.setText(MKProvider.getMK().mixer_manager.getName());
@@ -353,47 +211,6 @@ public class MixerEditActivity extends BaseActivity implements OnItemSelectedLis
         return super.onOptionsItemSelected(item);
     }
 
-
-    @Override
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-                               long arg3) {
-        try {
-            Log.d("on item sel ");
-            if (arg1 != null) {
-                // view has a parent
-                if (arg1.getParent() != null) {/*
-			Log.d("D","item with parent" + arg1.getParent());
-			if (arg1.getParent() instanceof Spinner)  {
-				Spinner spin=(Spinner)arg1.getParent();
-				
-				Log.d("D","Spinner tag" + spin.getTag());
-				
-				if (arg2==0)
-					edit_texts[(Integer)(spin.getTag())].setEnabled(true);
-				else
-				{
-					int tag_id=(Integer)(spin.getTag());
-					edit_texts[tag_id].setEnabled(false);
-					edit_texts[tag_id].setText("" + (250+arg2));
-					MKProvider.getMK().params.set_field_from_act(MKProvider.getMK().params.field_positions[act_topic][tag_id], 250+arg2);
-				}
-			}*/
-
-                }
-            }
-        } catch (Exception e) {
-            Log.d("DD" + e);
-        }
-
-        //arg1.getParent()
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getSupportMenuInflater().inflate(R.menu.mixer, menu);
@@ -445,18 +262,4 @@ public class MixerEditActivity extends BaseActivity implements OnItemSelectedLis
 
     }
 
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-
-    }
 }
